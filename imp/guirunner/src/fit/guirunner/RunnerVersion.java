2829e1123f02db1ce7e0e8da05fe7af96cf53ca0
mbusik <mbusik>
2008-06-02 23:12:16 +0000

    Build 008 - RC1

--- imp/guirunner/src/fit/guirunner/RunnerVersion.java
+++ imp/guirunner/src/fit/guirunner/RunnerVersion.java
@@ -1,6 +1,6 @@
 package fit.guirunner;
 
 public interface RunnerVersion {
-  final String RUNNER_VERSION = "005";
-  final String RUNNER_ID = "$Id: RunnerVersion.java,v 1.4 2008/05/25 18:59:13 mbusik Exp $";
+  final String RUNNER_VERSION = "008";
+  final String RUNNER_ID = "$Id: RunnerVersion.java,v 1.5 2008/06/02 23:12:16 mbusik Exp $";
 }
